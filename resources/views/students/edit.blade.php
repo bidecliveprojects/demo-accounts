44d2cd4bf8f4cb6cc7602ab04700da82e316d0e2
iamshahfaisal <faisal@bidecsol.com>
2025-11-26 11:08:39 +0500

    fix

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -210,7 +210,7 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                             <label>Home Address <span class="text-danger">*</span></label>
                             <input type="text" name="home_address"
                             class="form-control @error('home_address') border border-danger @enderror"
